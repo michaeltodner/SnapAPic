1e3e00cf5fe2e49b54686d44635102feaccf5a74
Michael T <michaeltodner@null.net>
2016-08-16 15:49:54 +0100

    added backend upload script

---
+++ chadgrab.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_FILES['imagedata']['tmp_name']))
+{
+	$newName = 'images/' . substr(md5(rand() . time()), 0, 20) . '.png';
+    $tf = fopen($newName, 'w');
+    fclose($tf);
+    move_uploaded_file($_FILES['imagedata']['tmp_name'], $newName);
+    echo 'http://www.starvin-marvin.com/' . $newName;
+}
+?>
